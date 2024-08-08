b50cb7a4cbe258fcb1833a61cd697f7ea284e456
sanjuan.jeancarlo@gmail.com <sanjuan.jeancarlo@gmail.com>
2024-08-08 12:27:23 +0800

    partial fix #1

--- reader/src/textToSpeech/treeWalker.ts
+++ reader/src/textToSpeech/treeWalker.ts
@@ -1,6 +1,11 @@
+import { BOOKMARK_CLASS } from "../bookmarks";
+
 function skipEmpty(n:Node) {
+
     return n.textContent && 
-        /\w/g.test(n.textContent) ? 
+        /\w/g.test(n.textContent) && 
+        // Skip bookmarked element, but its child will still be checked
+        !(n as HTMLElement)?.classList?.contains(BOOKMARK_CLASS) ? 
         NodeFilter.FILTER_ACCEPT: 
         NodeFilter.FILTER_SKIP
 }
